9db807a2100ab1a4e798ff49eabe3c96f16c5f36
micho1999 <149084650+micho1999@users.noreply.github.com>
2023-11-02 16:55:51 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,58 +1,52 @@
 function getUserMedia(constraints) {
-  // if Promise-based API is available, use it
-  if (navigator.mediaDevices) {
-    return navigator.mediaDevices.getUserMedia(constraints);
-  }
-    
-  // otherwise try falling back to old, possibly prefixed API...
-  var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-    navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    
-  if (legacyApi) {
-    // ...and promisify it
-    return new Promise(function (resolve, reject) {
-      legacyApi.bind(navigator)(constraints, resolve, reject);
-    });
-  }
-}
-
-function getStream(type) {
-  if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
-    !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
-    alert('User Media API not supported.');
-    return;
-  }
-
-  var constraints = {};
-  constraints[type] = true;
-
-  getUserMedia(constraints)
-    .then(function (stream) {
-      var mediaControl = document.querySelector('.' + type);
+    // if Promise-based API is available, use it
+    if (navigator.mediaDevices) {
+      return navigator.mediaDevices.getUserMedia(constraints);
+    }
       
-      if ('srcObject' in mediaControl) {
-        mediaControl.srcObject = stream;
-      } else if (navigator.mozGetUserMedia) {
-        mediaControl.mozSrcObject = stream;
-      } else {
-        mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
-      }
+    // otherwise try falling back to old, possibly prefixed API...
+    var legacyApi = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+      navigator.mozGetUserMedia || navigator.msGetUserMedia;
       
-      mediaControl.play();
-    })
-    .catch(function (err) {
-      alert('Error: ' + err);
-    });
-}
-
-document.addEventListener("DOMContentLoaded", function() {
-});
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("./serviceWorker.js", { scope: "./" })
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
+    if (legacyApi) {
+      // ...and promisify it
+      return new Promise(function (resolve, reject) {
+        legacyApi.bind(navigator)(constraints, resolve, reject);
+      });
+    }
+  }
+  
+  function getStream(type) {
+    if (!navigator.mediaDevices && !navigator.getUserMedia && !navigator.webkitGetUserMedia &&
+      !navigator.mozGetUserMedia && !navigator.msGetUserMedia) {
+      alert('User Media API not supported.');
+      return;
+    }
+  
+    var constraints = {};
+    constraints[type] = true;
+  
+    getUserMedia(constraints)
+      .then(function (stream) {
+        var mediaControl = document.querySelector('.' + type);
+        
+        if ('srcObject' in mediaControl) {
+          mediaControl.srcObject = stream;
+        } else if (navigator.mozGetUserMedia) {
+          mediaControl.mozSrcObject = stream;
+        } else {
+          mediaControl.src = (window.URL || window.webkitURL).createObjectURL(stream);
+        }
+        
+        mediaControl.play();
+      })
+      .catch(function (err) {
+        alert('Error: ' + err);
+      });
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
   });
-}
+  
+
+  
