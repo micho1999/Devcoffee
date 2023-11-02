0f386dbd8c3a92d1ae225b67500e31da5637fc3b
micho1999 <149084650+micho1999@users.noreply.github.com>
2023-11-02 18:30:02 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -54,3 +54,23 @@ if ("serviceWorker" in navigator) {
       });
   }
   
+function takePhoto() {
+  if (!('ImageCapture' in window)) {
+    alert('ImageCapture is not available');
+    return;
+  }
+  
+  if (!theStream) {
+    alert('Grab the video stream first!');
+    return;
+  }
+  
+  var theImageCapturer = new ImageCapture(theStream.getVideoTracks()[0]);
+
+  theImageCapturer.takePhoto()
+    .then(blob => {
+      var theImageTag = document.getElementById("imageTag");
+      theImageTag.src = URL.createObjectURL(blob);
+    })
+    .catch(err => alert('Error: ' + err));
+}
