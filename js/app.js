92b35f08d3f073f9ae3cb3cce0f8c51bd3443f2d
micho1999 <149084650+micho1999@users.noreply.github.com>
2023-11-02 16:59:15 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function() {
+      navigator.serviceWorker
+        .register("./serviceWorker.js", { scope: "./" })
+        .then(res => console.log("service worker registered"))
+        .catch(err => console.log("service worker not registered", err));
+    });
+  }
 function getUserMedia(constraints) {
     // if Promise-based API is available, use it
     if (navigator.mediaDevices) {
