acf93f7c7fc4cfb439730ed983dc8aa38edcdbd5
micho1999 <149084650+micho1999@users.noreply.github.com>
2023-12-07 13:12:06 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -38,13 +38,27 @@ function loadSavedData() {
       const cell2 = newRow.insertCell(1);
       const cell3 = newRow.insertCell(2);
       const cell4 = newRow.insertCell(3);
+      const cell5 = newRow.insertCell(4); // Hinzufügen einer neuen Zelle für Balance
 
       cell1.textContent = expense.description;
       cell2.textContent = expense.date;
       cell3.textContent = expense.category;
       cell4.textContent = expense.number.toFixed(2);
+      cell5.textContent = calculateBalance(expensesArray.indexOf(expense));
     });
   }
+}
+
+// Funktion zum Berechnen des Balance-Betrags
+function calculateBalance(index) {
+  const expenses = JSON.parse(localStorage.getItem('expenses'));
+  let balance = 10000; // Standardbetrag
+
+  for (let i = 0; i <= index; i++) {
+    balance -= expenses[i].number;
+  }
+
+  return balance.toFixed(2);
 }
 
 // Funktion zum Aktualisieren des Betrags in der Tabelle
@@ -58,10 +72,9 @@ function updateAmountInTable(lastAmount, newAmount) {
   const updatedAmount = currentAmount - lastAmount;
   cells[3].textContent = updatedAmount.toFixed(2);
 
-  // Aktualisiere den Betrag der letzten Zeile mit dem neuen Betrag
-  const newCells = rows[rows.length - 1].getElementsByTagName('td');
-  const updatedNewAmount = parseFloat(newCells[3].textContent) - newAmount;
-  newCells[3].textContent = updatedNewAmount.toFixed(2);
+  const index = rows.length - 1;
+  const newCells = rows[index].getElementsByTagName('td');
+  newCells[4].textContent = calculateBalance(index - 1);
 }
 
 // Restlicher Code bleibt unverändert
@@ -85,11 +98,13 @@ document.getElementById('expenseForm').addEventListener('submit', function(event
   const cell2 = newRow.insertCell(1);
   const cell3 = newRow.insertCell(2);
   const cell4 = newRow.insertCell(3);
+  const cell5 = newRow.insertCell(4);
 
   cell1.textContent = description;
   cell2.textContent = date;
   cell3.textContent = category;
   cell4.textContent = number;
+  cell5.textContent = calculateBalance(tableBody.rows.length - 2);
 
   document.getElementById('expenseForm').reset();
 });
