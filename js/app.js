99f98f56eb43ea1d0f381dc51147a282df9ca545
micho1999 <149084650+micho1999@users.noreply.github.com>
2024-01-22 20:34:34 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -67,56 +67,133 @@ function getPic(){
 
 }
 
+if ('storage' in navigator && 'estimate' in navigator.storage) {
+  navigator.storage.estimate()
+    .then(estimate => {
+      document.getElementById('usage').innerHTML = estimate.usage;
+      document.getElementById('quota').innerHTML = estimate.quota;
+      document.getElementById('percent').innerHTML = (estimate.usage * 100 / estimate.quota);
+    });
+}
+
+if ('storage' in navigator && 'persisted' in navigator.storage) {
+  navigator.storage.persisted()
+    .then(persisted => {
+      document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+    });
+}
+
+function requestPersistence() {
+  if ('storage' in navigator && 'persist' in navigator.storage) {
+    navigator.storage.persist()
+      .then(persisted => {
+        document.getElementById('persisted').innerHTML = persisted ? 'persisted' : 'not persisted';
+      });
+  }
+}
+
+function getReadFile(reader, i) {
+  return function () {
+    var li = document.querySelector('[data-idx="' + i + '"]');
+
+    li.innerHTML += 'File starts with "' + reader.result.substr(0, 25) + '"';
+  }
+}
+
+function readFiles(files) {
+  document.getElementById('count').innerHTML = files.length;
+
+  var target = document.getElementById('target');
+  target.innerHTML = '';
+
+  for (var i = 0; i < files.length; ++i) {
+    var item = document.createElement('li');
+    item.setAttribute('data-idx', i);
+    var file = files[i];
+
+    var reader = new FileReader();
+    reader.addEventListener('load', getReadFile(reader, i));
+    reader.readAsText(file);
+
+    item.innerHTML = '' + file.name + ', ' + file.type + ', ' + file.size + ' bytes, last modified ' + file.lastModifiedDate + '';
+    target.appendChild(item);
+  };
+}
+
+async function writeFile() {
+  if (!window.chooseFileSystemEntries) {
+    alert('Native File System API not supported');
+    return;
+  }
+  
+  const target = document.getElementById('target');
+  target.innerHTML = 'Opening file handle...';
+  
+  const handle = await window.chooseFileSystemEntries({
+    type: 'save-file',
+  });
+  
+  const file = await handle.getFile()
+  const writer = await handle.createWriter();
+  await writer.write(0, 'Hello world from What Web Can Do!');
+  await writer.close()
+  
+  target.innerHTML = 'Test content written to ' + file.name + '.';
+}
+
 if ("serviceWorker" in navigator) {
+  let version = 3;
   window.addEventListener("load", function() {
     navigator.serviceWorker
      .register("./serviceWorker.js",{ scope: "./" })
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err));
+      .then(res => console.log("Service worker registered !" + "version:" + version))
+      .catch(err => console.log("Service worker not registered !" + "version:" + version, err));
   });
 }
 
+if ('localStorage' in window || 'sessionStorage' in window) {
+  var selectedEngine;
 
-function startDrag(e) {
-  this.ontouchmove = this.onmspointermove = moveDrag;
+  var logTarget = document.getElementById('target');
+  var valueInput = document.getElementById('value');
 
-  this.ontouchend = this.onmspointerup = function () {
-    this.ontouchmove = this.onmspointermove = null;
-    this.ontouchend = this.onmspointerup = null;
+  var reloadInputValue = function () {
+  console.log(selectedEngine, window[selectedEngine].getItem('myKey'))
+    valueInput.value = window[selectedEngine].getItem('myKey') || '';
+  }
+  
+  var selectEngine = function (engine) {
+    selectedEngine = engine;
+    reloadInputValue();
+  };
+
+  function handleChange(change) {
+    var timeBadge = new Date().toTimeString().split(' ')[0];
+    var newState = document.createElement('p');
+    newState.innerHTML = '' + timeBadge + ' ' + change + '.';
+    logTarget.appendChild(newState);
   }
 
-  var pos = [this.offsetLeft, this.offsetTop];
-  var that = this;
-  var origin = getCoors(e);
-
-  function moveDrag(e) {
-    var currentPos = getCoors(e);
-    var deltaX = currentPos[0] - origin[0];
-    var deltaY = currentPos[1] - origin[1];
-    this.style.left = (pos[0] + deltaX) + 'px';
-    this.style.top = (pos[1] + deltaY) + 'px';
-    return false; // cancels scrolling
+  var radios = document.querySelectorAll('#selectEngine input');
+  for (var i = 0; i < radios.length; ++i) {
+    radios[i].addEventListener('change', function () {
+      selectEngine(this.value)
+    });
   }
+  
+  selectEngine('localStorage');
 
-  function getCoors(e) {
-    var coors = [];
-    if (e.targetTouches && e.targetTouches.length) {
-      var thisTouch = e.targetTouches[0];
-      coors[0] = thisTouch.clientX;
-      coors[1] = thisTouch.clientY;
-    } else {
-      coors[0] = e.clientX;
-      coors[1] = e.clientY;
+  valueInput.addEventListener('keyup', function () {
+    window[selectedEngine].setItem('myKey', this.value);
+  });
+
+  var onStorageChanged = function (change) {
+    var engine = change.storageArea === window.localStorage ? 'localStorage' : 'sessionStorage';
+    handleChange('External change in ' + engine + ': key ' + change.key + ' changed from ' + change.oldValue + ' to ' + change.newValue + '');
+    if (engine === selectedEngine) {
+      reloadInputValue();
     }
-    return coors;
   }
-}
 
-var elements = document.querySelectorAll('.test-element');
-[].forEach.call(elements, function (element) {
-  element.ontouchstart = element.onmspointerdown = startDrag;
-});
-
-document.ongesturechange = function () {
-  return false;
+  window.addEventListener('storage', onStorageChanged);
 }
