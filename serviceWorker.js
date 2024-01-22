8090a3f669a0cd9f0f36f54cc618eb4b4839ebee
micho1999 <149084650+micho1999@users.noreply.github.com>
2024-01-22 20:33:38 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,22 +1,37 @@
-installEvent.waitUntil(
-  caches.open(staticDevCoffee).then(cache => {
-    return cache.addAll(assets)
-      .then(() => {
-        let version = 3;
-        console.log("Service worker installed!");
-        console.log("version: " + version);
-      })
-      .catch(error => {
-        console.error("Cache installation error:", error);
-      });
-  })
-);
+const staticDevCoffee = "dev-coffee-site-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/images/coffee1.jpg",
+  "/images/coffee2.jpg",
+  "/images/coffee3.jpg",
+  "/images/coffee4.jpg",
+  "/images/coffee5.jpg",
+  "/images/coffee6.jpg",
+  "/images/coffee7.jpg",
+  "/images/coffee8.jpg",
+  "/images/coffee9.jpg"
+];
 
-self.addEventListener("activate", function (e) {
-  let version = 3;
-  console.log("Service worker activated!");
-  console.log("version: " + version);
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets);
+      let version = 3;
+      console.log("Service worker installed !");
+      console.log("version:" + version);
+    })
+  );
 });
+
+self.addEventListener("activate", function(e) {
+    let version = 3;
+    console.log("Service worker activated !");
+    console.log("version:" + version);
+  }
+);
 
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
