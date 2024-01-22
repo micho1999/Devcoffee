98edbe7827dbb426fe9130d8e3d7b7d2529679c4
micho1999 <149084650+micho1999@users.noreply.github.com>
2024-01-22 20:24:22 +0100

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,24 +1,27 @@
 installEvent.waitUntil(
-    caches.open(staticDevCoffee).then(cache => {
-      cache.addAll(assets);
-      let version = 3;
-      console.log("Service worker installed !");
-      console.log("version:" + version);
-    })
-  );
-});
+  caches.open(staticDevCoffee).then(cache => {
+    cache.addAll(assets);
+    let version = 3;
+    console.log("Service worker installed !");
+    console.log("version:" + version);
+  })
+);
+
 
 self.addEventListener("activate", function(e) {
-    let version = 3;
-    console.log("Service worker activated !");
-    console.log("version:" + version);
-  }
+  let version = 3;
+  console.log("Service worker activated !");
+  console.log("version:" + version);
+}
 );
 
 self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(res => {
-      //console.log("Fetching service worker")
-      return res || fetch(fetchEvent.request);
-    })
-  );
+fetchEvent.respondWith(
+  caches.match(fetchEvent.request).then(res => {
+    //console.log("Fetching service worker")
+    return res || fetch(fetchEvent.request);
+  })
+)
+}
+
+)
